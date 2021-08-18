98202a8c0578640a3aca817519d430f0f69c847c
Milos Danilov <milosdanilov@gmail.com>
2021-08-19 00:13:52 +0200

    chore: demo

--- projects/angular-bulma/src/public-api.ts
+++ projects/angular-bulma/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of angular-bulma
- */
-
-export * from './lib/angular-bulma.service';
-export * from './lib/angular-bulma.component';
-export * from './lib/angular-bulma.module';
+export const version = 0;
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ColumnsModule } from 'angular-bulma/columns';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColumnsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
