4ca9f80f81c208abc71456c1433075aa88678cc1
Milos Danilov <milosdanilov@gmail.com>
2021-08-21 15:16:43 +0200

    feat(column): generate different sizes

--- projects/angular-bulma/src/lib/columns/column.directive.ts
+++ projects/angular-bulma/src/lib/columns/column.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, HostBinding, Input } from '@angular/core';
-import { ColumnSize } from './column.interface';
+
+import { ColumnSize, NarrowType } from './column.interface';
 
 @Directive({
   selector: '[blColumn], bl-column',
@@ -9,18 +10,56 @@ export class ColumnDirective {
   size!: ColumnSize;
 
   @Input()
+  sizeMobile!: ColumnSize;
+
+  @Input()
+  sizeTablet!: ColumnSize;
+
+  @Input()
+  sizeDesktop!: ColumnSize;
+
+  @Input()
+  sizeWidescreen!: ColumnSize;
+
+  @Input()
+  sizeFullHD!: ColumnSize;
+
+  @Input()
   offset!: ColumnSize;
+
+  @Input()
+  narrow!: NarrowType;
 
   @HostBinding('class')
   get elementClass() {
-    return ['column', this.sizeClass, this.offsetClass].join(' ');
-  }
-
-  get sizeClass() {
-    return this.size ? `is-${this.size}` : '';
+    return [
+      'column',
+      ...this.generateSizes(),
+      this.offsetClass,
+      this.narrowClass,
+    ].join(' ');
   }
 
   get offsetClass() {
     return this.offset ? `is-offset-${this.offset}` : '';
+  }
+
+  get narrowClass() {
+    if (this.narrow === '') {
+      return 'is-narrow';
+    }
+
+    return this.narrow ? `is-narrow-${this.narrow}` : '';
+  }
+
+  private generateSizes() {
+    return [
+      this.size ? `is-${this.size}` : '',
+      this.sizeMobile ? `is-${this.sizeMobile}-mobile` : '',
+      this.sizeTablet ? `is-${this.sizeTablet}-tablet` : '',
+      this.sizeDesktop ? `is-${this.sizeDesktop}-desktop` : '',
+      this.sizeWidescreen ? `is-${this.sizeWidescreen}-widescreen` : '',
+      this.sizeFullHD ? `is-${this.sizeFullHD}-fullhd` : '',
+    ];
   }
 }
--- projects/angular-bulma/src/lib/columns/column.interface.ts
+++ projects/angular-bulma/src/lib/columns/column.interface.ts
@@ -13,3 +13,13 @@ type _ColumnSize =
   | 'one-fifth';
 
 type Grid = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+export type NarrowType = '' | Breakpoint;
+
+type Breakpoint =
+  | 'mobile'
+  | 'tablet'
+  | 'touch'
+  | 'desktop'
+  | 'widescreen'
+  | 'fullhd';
