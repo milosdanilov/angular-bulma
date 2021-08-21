54d7551a0c5274785d172d26f90998e554d14ba1
Milos Danilov <milosdanilov@gmail.com>
2021-08-21 16:33:09 +0200

    fix(column): grid should be string literals

--- projects/angular-bulma/src/lib/columns/column.interface.ts
+++ projects/angular-bulma/src/lib/columns/column.interface.ts
@@ -12,7 +12,7 @@ type _ColumnSize =
   | 'one-quarter'
   | 'one-fifth';
 
-type Grid = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+type Grid = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
 
 export type NarrowType = '' | Breakpoint;
 
